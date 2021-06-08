82be30623d95ca4f49522d1701abdbaeca504b9b
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-06-08 13:13:12 +0530

    Explore 🚀

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
-import './App.css';
+import img from './Images/blob.svg'
+import './App.scss';
 
 function App() {
   return (
 
 <>
-
+<div className="main">
+  <img src={img} alt="blob" />
+<h1> Explore </h1>
+</div>
 </>
  
   );
